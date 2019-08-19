e755affb7f64987515a4123f79b47fe70a7d7cd5
Leif Johansson <leifj@sunet.se>
2019-08-19 14:13:46 +0200

    cut 1.0.2

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,13 +2,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new BundleAnalyzerPlugin(),
     new webpack.EnvironmentPlugin({
       LOGLEVEL: 'error'
     })
